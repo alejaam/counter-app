835afa0ae7b691eae55db1964691b9f55d68e266
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-11-11 00:35:37 -0600

    Impresión de variables en el HTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,13 +1,15 @@
 // Functinoal Components
-import React from 'react';
+import React from "react";
 // import React, { Fragment } from 'react';
 const PrimeraApp = () => {
+    const saludo = "Hola Mundo";
     return (
         <>
-            <h1>Hola Mundo</h1>
-            <p>Mi primera aplicación</p>
+            <h1> {saludo} </h1>
+            {/* <h1> {JSON.stringify{saludo, null, 3}} </h1> */}
+            <p> Mi primera aplicación </p>
         </>
-        ); 
-}
+    );
+};
 
 export default PrimeraApp;
