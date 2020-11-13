b01028106c0c95a3104edaf77d762d98ac437176
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-11-12 20:46:28 -0600

    Default props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,23 +1,25 @@
 // Functinoal Components
 import React from "react";
 import PropTypes from "prop-types";
-
 // import React, { Fragment } from 'react';
-const PrimeraApp = ({ saludo }) => {
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
     
-
-
     return (
         <>
             <h1> {saludo} </h1>
             {/* <h1> {JSON.stringify{saludo, null, 3}} </h1> */}
-            <p> Mi primera aplicación </p>
+            <p> {subtitulo} </p>
         </>
     );
 };
 
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: "Soy un subtitulo"
 }
 
 export default PrimeraApp;
