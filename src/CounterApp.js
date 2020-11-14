633140a02883aa34d8a4f6fb6aed55ed91984897
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-11-14 01:06:04 -0600

    Funciones handleSubstract y handleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,13 +2,18 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
-    // handleAdd
+    // handleSubstract
+    const handleSubstract = () => {
+        setCounter((c) => (c > 0)? c - 1 : 0);
+    }
+    const handleReset = () => {
+        setCounter((c) => c = value);
+    }
     const handleAdd = () => {
-        // setCounter(counter + 1);
         setCounter((c) => c + 1);
     }
     return (
@@ -16,6 +21,8 @@ const CounterApp = ({ value }) => {
             <h1>CounterApp</h1>
             {/* <h1> {JSON.stringify{saludo, null, 3}} </h1> */}
             <h2> {counter} </h2>
+            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
             <button onClick={ handleAdd }>+1</button>
         </>
     );
