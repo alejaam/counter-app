85eda8c1a51fe498019e3cc101c52383c4765410
Alejandro Alamar <alejaam@users.noreply.github.com>
2020-11-14 09:26:58 -0600

    Jest - Expect - toBe

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,6 +1,13 @@
-test('Debe ser true', () => {
-    const isActive = true;
-    if (isActive) {
-        throw new Error('No esta activo');
-    }
+describe('Pruebas en el archivo demo.test.js', () => {
+
+    test('Deben ser iguales los string', () => {
+        // 1.- Inicialización
+        const mensaje = "Hola mundo";
+    
+        // 2.- Estimulo
+        const mensaje2 = `Hola mundo!`;
+
+        // 3.- Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    })
 });
