913eda0b4f8289a01b383ecf14c0ea86684a2aa6
Alejandro <ale.jaam7@gmail.com>
2020-11-27 23:24:17 -0600

    Enzyme testing unit

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -2,12 +2,22 @@ const { render } = require("@testing-library/react");
 const { default: PrimeraApp } = require("../PrimeraApp");
 
 import React from 'react';
+import { shallow } from 'enzyme';
+// import PrimeraApp from '../PrimeraApp';
+
 
 describe('Pruebas en <PrimeraApp />', () => {
-    test('Debe mostrar el mensaje "Hola, Soy Goku"', () => {
-        const saludo = 'Hola, soy Goku';
-        const {getByText} = render(<PrimeraApp saludo={saludo} />);
+    // test('Debe mostrar el mensaje "Hola, Soy Goku"', () => {
+    //     const saludo = 'Hola, soy Goku';
+    //     const {getByText} = render(<PrimeraApp saludo={saludo} />);
+
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // })
+    test('Debe mostrar <PrimeraApp> correctamente', () => { 
+        const saludo = 'Hola, soy goku'
+        const wrapper = shallow(<PrimeraApp saludo={saludo} />);
         
-        expect(getByText(saludo)).toBeInTheDocument();
-    })
-})
+        expect(wrapper).toMatchSnapshot();
+    });
+
+});
